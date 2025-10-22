62b3dd8b637a039702c9d578bd236f2a715e1433
Yudi Omaki <yudi.omaki@sou.inteli.edu.br>
2025-10-22 20:52:08 -0300

    Create script.cpp

---
+++ script.cpp
@@ -0,0 +1,20 @@
+int pinoNoRC=0; 
+int valorLido = 0;
+float tensaoCapacitor = 0, tensaoResistor;
+unsigned long time; 
+void setup(){ 
+Serial.begin(9600); 
+} 
+void loop() { 
+	time=millis(); 
+	valorLido=analogRead(pinoNoRC); 
+	tensaoResistor=(valorLido*5.0/1023); // 5.0V / 1023 degraus = 0.0048876 
+	tensaoCapacitor = abs(5.0-tensaoResistor);
+ 	Serial.print(time); //imprime o conteúdo de time no MONITOR SERIAL
+    Serial.print(" "); 
+  	Serial.print(tensaoResistor);
+  	Serial.print(" ");
+  	Serial.println(tensaoCapacitor); 
+	delay(400); 
+}
+
